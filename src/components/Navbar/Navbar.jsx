105dc551100e5223040487fc9dbe0ed576181859
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-08-20 16:02:36 +0200

    Création et stylisation de la navigation

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import './Navbar.css'
+
+
+function Navbar() {
+  return (
+    
+    <nav>
+      <div className='logo'>Logo</div>
+      
+      <a href="#accueil">Accueil</a>
+      <a href="#a-propos">À propos</a>
+      <a href="#competences">Compétences</a>
+      <a href="#projets">Projets</a>
+      <a href="#contact">Contact</a>
+      <a href="#cv">Télécharger CV</a>
+      
+    </nav>
+  )
+}
+
+export default Navbar
