7e1c4c26790bf9cfabe2e48e51f12ead59e52642
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-08-21 15:45:43 +0200

    img hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,5 @@
 import './Hero.css';
 import heroImg from "../../assets/hero-img.jpg";
-import './Hero.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
-
 
 function Hero() {
   return (
@@ -23,18 +17,25 @@ function Hero() {
           <a href="#contact" className="btn btn-secondary">Me contacter</a>
         </div>
 
-      <div className="hero-socials">
-          <a href="https://github.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faGithub} size="xl" />
+          <div className="hero-socials">
+          <a href="https://github.com" target="_blank" rel="noreferrer" aria-label="GitHub">
+            <i className="fa-brands fa-github"></i>
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} size="xl" />
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer" aria-label="LinkedIn">
+            <i className="fa-brands fa-linkedin"></i>
           </a>
-          <a href="mailto:votre-email@example.com">
-            <FontAwesomeIcon icon={faEnvelope} size="xl" />
+          <a href="mailto:ton-email@example.com" aria-label="Email">
+            <i className="fa-regular fa-envelope"></i>
           </a>
         </div>
       </div>
+
+      <div className="hero-image-container">
+        
+        <img src={heroImg} alt="Samira's Toolkit" className="hero-image" />
+      </div>
+
+      
     </section>
   );
 }
