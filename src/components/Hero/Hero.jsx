c62ee3f37295ed6384581bada184764931dd73ea
boukhanoufa <arimasboukhanoufa@gmail.com>
2026-08-21 11:06:42 +0200

    feat(icons): ajoute et intègre Font Awesome dans le projet-Ajout des icônes de réseaux sociaux et de contact dans le composant hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,17 @@
 import './Hero.css';
 import heroImg from "../../assets/hero-img.jpg";
+import './Hero.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
 
 function Hero() {
   return (
     <section className="hero">
       <div className="hero-content">
-        <p className="hero-intro">Bonjour, je suis</p>
+        <p className="hero-intro">Hello ! je suis</p>
         <h1 className="hero-name">BOUKHANOUFA SAMIRA</h1>
         <h2 className="hero-title">Intégratrice Web</h2>
         <p className="hero-description">
@@ -17,22 +23,17 @@ function Hero() {
           <a href="#contact" className="btn btn-secondary">Me contacter</a>
         </div>
 
-        <div className="hero-socials">
-          <a href="https://github.com" target="_blank" rel="noreferrer" aria-label="GitHub">
-            <i className="fa-brands fa-github"></i>
+      <div className="hero-socials">
+          <a href="https://github.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faGithub} size="xl" />
           </a>
-          <a href="https://linkedin.com" target="_blank" rel="noreferrer" aria-label="LinkedIn">
-            <i className="fa-brands fa-linkedin"></i>
+          <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="xl" />
           </a>
-          <a href="mailto:ton-email@example.com" aria-label="Email">
-            <i className="fa-regular fa-envelope"></i>
+          <a href="mailto:votre-email@example.com">
+            <FontAwesomeIcon icon={faEnvelope} size="xl" />
           </a>
         </div>
-      </div>
-
-      <div className="hero-image-container">
-        
-        <img src={heroImg} alt="Samira's Toolkit" className="hero-image" />
       </div>
     </section>
   );
